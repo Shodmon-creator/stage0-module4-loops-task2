8d60f6b9935472d81d95c7c33e1c90bc0de77fd8
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2022-11-02 11:52:04 +0500

    second attempt

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -1,11 +1,7 @@
 package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
-    public void buildPhrase(char... chars) {
-
-        while (chars.length>3){
-            System.out.println(chars);
-        }
+    public void buildPhrase(char... chars){
 
     }
 }
